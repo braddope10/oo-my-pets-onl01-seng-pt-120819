bc06102d1ab3cf34756aac2c26079906c550b69a
Bradley Lopez <bradleylopezherrera@gmail.com>
2020-01-27 22:14:40 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,113 @@
 class Owner
-  # code goes here
+  
+  attr_reader :name, :species
+  
+  @@all = []
+  @@pets = {:cats => [], :dogs => []}
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+  
+  def name
+    @name
+  end
+  
+  def species
+    @species
+  end
+  
+  def say_species
+    "I am a #{@species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats
+    @@pets
+  end
+    
 end
+
+
+
+#   class Owner
+#   attr_reader :pets, :dog, :cat
+#   attr_accessor  :species, :mood
+#   attr_writer 
+
+#   @@all = []
+#   @@pets = {:dogs => [], :cats => []}
+
+#   def initialize(species)
+#     @species = species
+#     @name = name
+#     @@all << self
+#   end
+
+#   def self.all
+#     @@all
+#   end
+
+#   def self.count
+#     @@all.length
+#   end
+
+#   def self.reset_all
+#     @@all.clear
+#   end
+
+#   def say_species
+#     "I am a #{@species}."
+#   end
+
+#   def pets
+#     @@pets
+#   end
+
+#   def buy_cat(cat)
+#     @@pets[:cats] << Cat.new(cat)
+#   end
+
+#   def buy_dog(dog)
+#     @@pets[:dogs] << Dog.new(dog)
+#   end
+
+#   def walk_dogs
+#     @@pets[:dogs].each do |dog|
+#       dog.mood = 'happy'
+#     end
+#   end
+
+#   def play_with_cats
+#     @@pets[:cats].each do |cat|
+#       cat.mood = 'happy'
+#     end
+#   end
+
+#   def list_pets
+#     "I have #{pets[:dogs].length} dog(s) and #{pets[:cats].length} cat(s)."
+#   end
+
+#   def sell_pets
+#     pets.each do |pet, arr|
+#       arr.map do |pet|
+#         pet.mood = 'nervous'
+#       end
+#       arr.clear
+#     end
+#   end
+
+# end
